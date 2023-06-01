0f1f185db289624dab1ba6063625074de82005c2
Utku Cengiz <cekicengiz01@gmail.com>
2023-06-01 11:52:09 +0300

    create page functions completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./app.css";
 import Create from "./components/Create";
 import Project from "./components/Project";
 function App() {
+  const [projects, setProjects] = useState([
+    {
+      id: 0,
+      projectName: "Deneme1",
+      colors: ["1b1b1b", "ababab"],
+    },
+    {
+      id: 1,
+      projectName: "Deneme1",
+      colors: ["0x0x0x", "1b1b1b", "ababab"],
+    },
+    {
+      id: 2,
+      projectName: "Deneme1",
+      colors: ["0x0x0x", "1b1b1b", "ababab"],
+    },
+  ]);
   return (
     <div className="bg-gray-900 h-screen">
       <div className="w-full text-center lg:text-left lg:pt-8 lg:pl-8">
@@ -10,8 +27,8 @@ function App() {
           Color Palette
         </h1>
       </div>
-      {/* <Create /> */}
-      <Project />
+      <Create projects={projects} setProjects={setProjects} />
+      {/* <Project /> */}
     </div>
   );
 }
--- src/components/Create.js
+++ src/components/Create.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Create() {
+function Create(props) {
+  const [project, setProject] = useState("");
+
+  const projectName = (e) => {
+    setProject(e.target.value);
+  };
+
+  function addProject() {
+    const newProject = {
+      id: props.projects.length,
+      projectName: project,
+      colors: ["000000", "ffffff"],
+    };
+    props.setProjects([...props.projects, newProject]);
+    setProject("");
+  }
   return (
     <div className="lg:flex">
       <div className="lg:w-1/3   border-r-2 border-gray-800 lg:pr-8 border-t-2 lg:border-b-2 mt-8 pt-8 lg:pl-8 lg:pb-8">
@@ -12,88 +27,41 @@ function Create() {
             type="text"
             id="createBtn"
             name="createBtn"
-            className="w-full bg-gray-800 text-center focus:outline-none h-10 text-white lg:text-left"
+            className="w-full bg-gray-800 text-center focus:outline-none h-10 text-white lg:text-left pl-2 text-2xl"
+            value={project}
+            onChange={projectName}
           />
-          {/* <label htmlFor="createBtn" className="absolute right-0 ">
-          ADD
-        </label> */}
         </div>
-        <div>
-          <h2 className="text-center text-2xl text-white mb-2 lg:text-3xl lg:text-left my-4">
-            Renk Ekle
-          </h2>
-          <div className="flex flex-col mx-2 ">
-            <div className="flex mb-6">
-              <span className="w-[60%] h-10 mr-4 bg-red-500"></span>
-              <input
-                type="text"
-                className=" bg-transparent border-solid border-2 border-gray-800 w-[40%] focus:outline-none text-white"
-              />
-            </div>
-            <div className="flex mb-6">
-              <span className="w-[60%] h-10 mr-4 bg-red-500"></span>
-              <input
-                type="text"
-                className=" bg-transparent border-solid border-2 border-gray-800 w-[40%] focus:outline-none text-white"
-              />
-            </div>
-            <div className="flex mb-6">
-              <span className="w-[60%] h-10 mr-4 bg-red-500"></span>
-              <input
-                type="text"
-                className=" bg-transparent border-solid border-2 border-gray-800 w-[40%] focus:outline-none text-white"
-              />
-            </div>
-            <div className="flex mb-6">
-              <span className="w-[60%] h-10 mr-4 bg-red-500"></span>
-              <input
-                type="text"
-                className=" bg-transparent border-solid border-2 border-gray-800 w-[40%] focus:outline-none text-white"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="text-center lg:text-left ">
-          <button className="text-2xl text-white lg:text-3xl ">Kaydet</button>
+
+        <div className="text-center lg:text-left mt-4">
+          <button
+            className="text-2xl text-white lg:text-3xl "
+            onClick={addProject}
+          >
+            Kaydet
+          </button>
         </div>
       </div>
-      <div className="lg:w-2/3 lg:pr-8 border-t-2 mt-8 pt-8 border-gray-800 lg:pl-8 lg:bg-slate-800">
+      <div className="lg:w-2/3 lg:p-8 border-t-2 mt-8 pt-8 border-gray-800  lg:bg-slate-800">
         <div className="text-center  lg:text-left">
           <h2 className="text-white text-2xl lg:text-3xl">Projelerim</h2>
         </div>
         <div className="lg:grid lg:grid-cols-2">
-          <div className="mt-5 mx-2 ">
-            <h3 className="text-2xl text-white mb-2 lg:text-3xl">Proje 1</h3>
-            <div className="flex flex-row  ">
-              <span className="flex-1 bg-red-500 h-10 lg:h-12"></span>
-              <span className="flex-1 bg-red-400 h-10 lg:h-12"></span>
-              <span className="flex-1 bg-red-300 h-10 lg:h-12"></span>
-              <span className="flex-1 bg-red-200 h-10 lg:h-12"></span>
-              <span className="flex-1 bg-red-100 h-10 lg:h-12"></span>
-              <span className="flex-1 bg-red-100 h-10 lg:h-12"></span>
-              <span className="flex-1 bg-red-100 h-10 lg:h-12"></span>
+          {props.projects.map((eleman) => (
+            <div className="mt-5 mx-2 " key={eleman.id}>
+              <h3 className="text-2xl text-white mb-2 lg:text-3xl">
+                {eleman.projectName}
+              </h3>
+              <div className="flex flex-row  ">
+                {eleman.colors.map((colors) => (
+                  <span
+                    className="flex-1  h-10 lg:h-12"
+                    style={{ backgroundColor: `#${colors}` }}
+                  ></span>
+                ))}
+              </div>
             </div>
-          </div>
-          <div className="mt-5 mx-2 ">
-            <h3 className="text-2xl text-white mb-2 lg:text-3xl">Proje 1</h3>
-            <div className="flex flex-row  ">
-              <span className="flex-1 bg-red-500 h-10 lg:h-12"></span>
-              <span className="flex-1 bg-red-400 h-10 lg:h-12"></span>
-              <span className="flex-1 bg-red-300 h-10 lg:h-12"></span>
-              <span className="flex-1 bg-red-200 h-10 lg:h-12"></span>
-              <span className="flex-1 bg-red-100 h-10 lg:h-12"></span>
-            </div>
-          </div>
-          <div className="mt-5 mx-2 ">
-            <h3 className="text-2xl text-white mb-2 lg:text-3xl">Proje 1</h3>
-            <div className="flex flex-row  ">
-              <span className="flex-1 bg-red-500 h-10 lg:h-12"></span>
-              <span className="flex-1 bg-red-400 h-10 lg:h-12"></span>
-              <span className="flex-1 bg-red-300 h-10 lg:h-12"></span>
-              <span className="flex-1 bg-red-200 h-10 lg:h-12"></span>
-              <span className="flex-1 bg-red-100 h-10 lg:h-12"></span>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
